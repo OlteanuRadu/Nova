d28dd500c55f37e2cd8d6aee5fa998ff7da0083c
Olteanu Radu <olteanurd@gmail.com>
2016-10-21 18:45:48 +0300

    initial working version

---
+++ Scripts/typings/tsd.d.ts
@@ -0,0 +1,13 @@
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="express/express.d.ts" />
+/// <reference path="body-parser/body-parser.d.ts" />
+/// <reference path="mongodb/mongodb.d.ts" />
+/// <reference path="mongoose/mongoose.d.ts" />
+/// <reference path="pluralize/pluralize.d.ts" />
+/// <reference path="linq/linq.d.ts" />
+/// <reference path="rx/rx.d.ts" />
+/// <reference path="chalk/chalk.d.ts" />
+
+
+
+
